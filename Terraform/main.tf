c060fd0cb082333c072038e7a0db42287589a2e7
devOps023 <ushachinnu5662gmail.com>
2022-06-16 10:51:41 +0530

    Modified main file

--- Terraform/main.tf
+++ Terraform/main.tf
@@ -1,23 +1,27 @@
-terraform{
-  backend "azurerm"{
-
-  }
-}
 
 provider "azurerm" {
+  version = "latest"
   features {}
 }
 
+terraform{
+  backend "azurerm"{
+    resource_group_name = var.resource_group
+    storage_account_name = var.storage_account
+    container_name = var.container_name
+
+  }
+}
 # Create a resource group
 resource "azurerm_resource_group" "RG" {
   name     = "${var.prefix}-RG"
-  location = "${var.location}"
+  location = var.location
 
 }
 
-resource "azurerm_virtual_network" "Vnet" {
+resource "azurerm_virtual_network" "vnet" {
   name                = "${var.prefix}-network"
-  address_space       = ["10.0.0.0/16"]
+  address_space       = var.address_space_vnet
   location            = azurerm_resource_group.RG.location
   resource_group_name = azurerm_resource_group.RG.name
 }
@@ -26,11 +30,11 @@ resource "azurerm_subnet" "subnet" {
   name                 = "${var.prefix}-subnet"
   resource_group_name  = azurerm_resource_group.RG.name
   virtual_network_name = azurerm_virtual_network.vnet.name
-  address_prefixes     = ["10.0.2.0/24"]
+  address_prefixes     = var.address_space_subnet
 }
 
 resource "azurerm_network_interface" "nic" {
-  name                = "${var.prefix}-nic"
+  name                = "${var.prefix}-nic}"
   location            = azurerm_resource_group.RG.location
   resource_group_name = azurerm_resource_group.RG.name
 
@@ -45,9 +49,9 @@ resource "azurerm_windows_virtual_machine" "VM" {
   name                = "${var.prefix}-VM"
   resource_group_name = azurerm_resource_group.RG.name
   location            = azurerm_resource_group.RG.location
-  size                = "Standard_F2"
-  admin_username      = "${var.username}"
-  admin_password      = "${var.password}"
+  size                = var.size
+  admin_username      = var.username
+  admin_password      = var.password
   network_interface_ids = [
     azurerm_network_interface.nic.id,
   ]
--- Terraform/variable.tf
+++ Terraform/variable.tf
@@ -1,19 +1,49 @@
 variable "prefix" {
-    description = "Prefix name"
+
   type        = string
 }
 
 variable "username" {
-  description = "Admin username"
+  
   type        = string
 }
 
 variable "password" {
-  description = "Admin password"
+ 
   type        = string
 }
 
 variable "location" {
-  description = "Azure location"
+ 
+  type        = string
+}
+
+variable "resource_group" {
+  
+  type        = string
+}
+
+variable "storage_account" {
+ 
+  type        = string
+}
+
+variable "container_name" {
+  
+  type        = string
+}
+
+variable "address_space_vnet" {
+  
+  type        = list
+}
+
+variable "address_space_subnet" {
+  
+  type        = list
+}
+
+variable "size" {
+  
   type        = string
 }
