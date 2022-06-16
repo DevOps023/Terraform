5eef2e69a53076ce9e6c75e4347f1ef1d35d4b15
usha <88317572+DevOps023@users.noreply.github.com>
2022-06-16 12:28:58 +0530

    Update main.tf

--- Terraform/main.tf
+++ Terraform/main.tf
@@ -34,7 +34,7 @@ resource "azurerm_subnet" "subnet" {
 }
 
 resource "azurerm_network_interface" "nic" {
-  name                = "${var.prefix}-nic}"
+  name                = "${var.prefix}-nic"
   location            = azurerm_resource_group.RG.location
   resource_group_name = azurerm_resource_group.RG.name
 
